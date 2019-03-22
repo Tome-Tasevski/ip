023c4565718a16fdfa88c374222021290a86b0ee
Aleksandra Koceva <aleksandra.koceva@it-labs.com>
2019-03-22 11:08:52 +0100

    fixed sp path

---
+++ Sensor/sp-saml/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace sp_saml.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
