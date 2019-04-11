dd62a5b1386950991ae72cc346d9f846c05c6e32
Elena Ristovska <elena.ristovska@it-labs.com>
2019-04-11 15:33:35 +0200

    Init AdminUI

---
+++ Sensor/IDS4Admin/ProfileService.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+
+namespace IDS4Admin
+{
+    public class ProfileService : IProfileService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IIdentityServerInteractionService _interaction;
+
+        public ProfileService(IHttpContextAccessor httpContextAccessor, IIdentityServerInteractionService interaction)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _interaction = interaction;
+        }
+
+        public Task GetProfileDataAsync(ProfileDataRequestContext context)
+        {
+            context.IssuedClaims.AddRange(context.Subject.Claims);
+            return Task.FromResult(0);
+        }
+
+        public Task IsActiveAsync(IsActiveContext context)
+        {
+
+            var features = _httpContextAccessor.HttpContext.Features.Get<IHttpRequestFeature>();
+            var returnurl = features.RawTarget;
+            var authcontext = _interaction.GetAuthorizationContextAsync(returnurl).Result;
+            
+            var currentrequesttenant = authcontext.Tenant.Split(".").First();
+            var user_tenant = context.Subject.Claims.First(x => x.Type.Equals("tenant")).Value;
+            if (!user_tenant.Equals(currentrequesttenant))
+            {
+                context.IsActive = false;
+
+            }
+            return Task.FromResult(context);
+        }
+    }
+}
---
+++ Sensor/IDS4Admin/Startup.cs
@@ -0,0 +1,192 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using IdentityExpress.Identity;
+using IdentityExpress.Manager.Api;
+using IdentityServer4.Configuration;
+using IdentityServer4.Services;
+using IDS4Admin.Data;
+using Microsoft.Extensions.Logging.Abstractions;
+using RSK.Audit.EF;
+using IdentityServer4Admin.Models;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.IdentityModel.Tokens;
+using Rsk.AspNetCore.Authentication.Saml2p;
+using RSK.Audit.EF;
+using RSK.IdentityServer4.AuditEventSink;
+
+namespace IDS4Admin
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public IHostingEnvironment Environment { get; }
+
+        public Startup(IConfiguration configuration, IHostingEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("Users")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddMvc();
+
+            services.Configure<IISOptions>(iis =>
+            {
+                iis.AuthenticationDisplayName = "Windows";
+                iis.AutomaticAuthentication = false;
+            });
+
+            const string connectionString = @"Data Source=.\SQLExpress;database=IdentityServer4.Quickstart.EntityFramework;trusted_connection=yes;";
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            var builder = services.AddIdentityServer(options =>
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+
+                options.UserInteraction = new UserInteractionOptions
+                {
+                    LogoutUrl = "/Account/Logout",
+                    LoginUrl = "/Account/Login",
+                    LoginReturnUrlParameter = "returnUrl"
+                };
+            })
+                .AddAspNetIdentity<ApplicationUser>()
+                // this adds the config data from DB (clients, resources, CORS)
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = db =>
+                        db.UseSqlite(connectionString,
+                            sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
+                // this adds the operational data from DB (codes, tokens, consents)
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = db =>
+                        db.UseSqlite(connectionString,
+                            sql => sql.MigrationsAssembly(migrationsAssembly));
+
+                    // this enables automatic token cleanup. this is optional.
+                    options.EnableTokenCleanup = true;
+                    // options.TokenCleanupInterval = 15; // interval in seconds. 15 seconds useful for debugging
+                })
+                .AddProfileService<ProfileService>()
+                .AddSigningCredential(new X509Certificate2("idsrv3test.pfx", "idsrv3test"))
+                .AddSamlPlugin(options => {
+                    options.Licensee = "Demo";
+                    options.LicenseKey = "eyJTb2xkRm9yIjowLjAsIktleVByZXNldCI6NiwiU2F2ZUtleSI6ZmFsc2UsIkxlZ2FjeUtleSI6ZmFsc2UsImF1dGgiOiJERU1PIiwiZXhwIjoiMjAxOS0wNS0xMVQwMTowMDowMC45Njc1MTI5KzAxOjAwIiwiaWF0IjoiMjAxOS0wNC0xMVQwMDowMDowMC4wMDA1MTI5Iiwib3JnIjoiREVNTyIsImF1ZCI6Mn0=.PKaOIARp3KB5GYWCoBXkJHOSqpGg2BcPpQX9/N/4SJ9rFyI3CLGILF/qrQzWBBUdTTKxMGU4LaPToCQE3XAF1M4Ikh8QCYNMsS2o5OlfO2+sqmVvI6Y8ucjMgwPnEigFW+q1+mZbWHlqPto0OsHhSjX8PgZb+g8nsWbGb5MLSAaM+8bLgcghizj3xZctr6QyOI0a9p9VThzPSNi4hKEyPBZ/EOjt7Qxh2R4TVsY9TnbeTIRT+P9yGXGQoJqmIIqVhlMu7v6Qe0hV1orgLKonJpqQVRsYUK/rl9ygyMV7lfB3KQ4k3EwqsUFF9OclMF+DZBNa1YOfqBmYnVebQWmpvkqxh/RiEiRrh+ERoGDNrOBgPbPlNj80dxy37rkqZSFyNg6su3F7v3ZyjFyS9kVha0HsnhkvN8Kz14myzokxwiBe2BVDy7ErzXajhP3q8a04SP6qL22mO9uBwAppHPD7UOU88+CC3GHVD5NmSjzMwN2sNzgExjOQ4dFDjvfcz9byilMUPCW1o3vRcIVA7CJx7F28ZYQFw/LuBlTPcZ9LlkWq1LptZR1KeusKX5vcz5QvWj7F+uO1fA2uwrUSdghGGkHbfLrh6OJmUFUD8HGm0N83ydLQKaEMMJgeA4T3ox19zycws7RdrZ6uLX2hTySCZ5xjf6eUu97QDxxd/LEnQvU=";
+                });
+            services.AddAuthentication(opt => opt.DefaultChallengeScheme = "adfs")
+                .AddOpenIdConnect("AAD", "Azure Active Directory", options =>
+                {
+                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                    options.SignOutScheme = IdentityServerConstants.SignoutScheme;
+                    options.Authority = "https://login.microsoftonline.com/common";
+                    options.ClientId = "f6a6b204-ff96-4013-8adb-7b1ef0bdda2a";
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = false
+                    };
+                    options.GetClaimsFromUserInfoEndpoint = true;
+                }).AddSaml2p("adfs", "AD authentication", options =>
+                {
+                    options.Licensee = "Demo";
+                    options.LicenseKey = "eyJTb2xkRm9yIjowLjAsIktleVByZXNldCI6NiwiU2F2ZUtleSI6ZmFsc2UsIkxlZ2FjeUtleSI6ZmFsc2UsImF1dGgiOiJERU1PIiwiZXhwIjoiMjAxOS0wNS0xMVQwMTowMDowMC45Njc1MTI5KzAxOjAwIiwiaWF0IjoiMjAxOS0wNC0xMVQwMDowMDowMC4wMDA1MTI5Iiwib3JnIjoiREVNTyIsImF1ZCI6Mn0=.PKaOIARp3KB5GYWCoBXkJHOSqpGg2BcPpQX9/N/4SJ9rFyI3CLGILF/qrQzWBBUdTTKxMGU4LaPToCQE3XAF1M4Ikh8QCYNMsS2o5OlfO2+sqmVvI6Y8ucjMgwPnEigFW+q1+mZbWHlqPto0OsHhSjX8PgZb+g8nsWbGb5MLSAaM+8bLgcghizj3xZctr6QyOI0a9p9VThzPSNi4hKEyPBZ/EOjt7Qxh2R4TVsY9TnbeTIRT+P9yGXGQoJqmIIqVhlMu7v6Qe0hV1orgLKonJpqQVRsYUK/rl9ygyMV7lfB3KQ4k3EwqsUFF9OclMF+DZBNa1YOfqBmYnVebQWmpvkqxh/RiEiRrh+ERoGDNrOBgPbPlNj80dxy37rkqZSFyNg6su3F7v3ZyjFyS9kVha0HsnhkvN8Kz14myzokxwiBe2BVDy7ErzXajhP3q8a04SP6qL22mO9uBwAppHPD7UOU88+CC3GHVD5NmSjzMwN2sNzgExjOQ4dFDjvfcz9byilMUPCW1o3vRcIVA7CJx7F28ZYQFw/LuBlTPcZ9LlkWq1LptZR1KeusKX5vcz5QvWj7F+uO1fA2uwrUSdghGGkHbfLrh6OJmUFUD8HGm0N83ydLQKaEMMJgeA4T3ox19zycws7RdrZ6uLX2hTySCZ5xjf6eUu97QDxxd/LEnQvU=";
+                    options.IdentityProviderOptions = new IdpOptions
+                    {
+                        EntityId = "https://adfs.it-labs.io/adfs/services/trust",
+                        SigningCertificate = new X509Certificate2("adfs-signing.cer"),
+                        SingleSignOnEndpoint = new SamlEndpoint("https://adfs.it-labs.io/adfs/ls/", SamlBindingTypes.HttpRedirect),
+                        SingleLogoutEndpoint = new SamlEndpoint("https://adfs.it-labs.io/adfs/ls/", SamlBindingTypes.HttpRedirect),
+                    };
+                    options.ServiceProviderOptions = new SpOptions
+                    {
+                        EntityId = "https://localhost:44374/saml",
+                        MetadataPath = "/saml/metadata",
+                        SignAuthenticationRequests = true,
+                        SigningCertificate = new X509Certificate2("testclient.pfx", "test")
+                    };
+
+                    options.SaveTokens = true;
+                    options.NameIdClaimType = "sub";
+                    options.CallbackPath = "/signin-saml";
+                    options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+                    options.TimeComparisonTolerance = 15;
+                });
+
+            ConfigureIdentityServerAuditing(services, connectionString);
+
+            if (Environment.IsDevelopment())
+            {
+                builder.AddDeveloperSigningCredential();
+            }
+            else
+            {
+                throw new Exception("need to configure key material");
+            }
+
+            services.UseAdminUI();
+            services.AddScoped<IdentityExpressDbContext, SqliteIdentityDbContext>();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseIdentityServer();
+
+            app.UseCommunityLogin();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+            app.UseAdminUI();
+        }
+        
+        public void ConfigureIdentityServerAuditing(IServiceCollection services, string auditConnectionString)
+        {
+            var dbContextOptionsBuilder = new DbContextOptionsBuilder<AuditDatabaseContext>();
+            RSK.Audit.AuditProviderFactory auditFactory = new AuditProviderFactory(dbContextOptionsBuilder.UseSqlite(auditConnectionString).Options);
+            var auditRecorder = auditFactory.CreateAuditSource("IdentityServer");
+            services.AddSingleton<IEventSink>(provider => new AuditSink(auditRecorder));
+
+            services.AddSingleton<IEventSink>(provider => new EventSinkAggregator(new NullLogger<EventSinkAggregator>())
+            {
+                EventSinks = new List<IEventSink>
+                {
+                    new AuditSink(auditRecorder)
+                }
+            });
+        }
+    }
+}
