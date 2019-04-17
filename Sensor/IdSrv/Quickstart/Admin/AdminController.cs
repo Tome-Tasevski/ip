94ce2f852e23648e6de237b6cded82069be1e15f
Elena Ristovska <elena.ristovska@it-labs.com>
2019-04-17 14:23:33 +0200

    Limited access on service providers

--- Sensor/IdSrv/Quickstart/Admin/AdminController.cs
+++ Sensor/IdSrv/Quickstart/Admin/AdminController.cs
@@ -28,7 +28,7 @@ namespace IdSrv.Quickstart.Admin
         }
 
         [HttpPost("new-client")]
-        public async Task<IActionResult> AddClient(NewClient client)
+        public async Task<IActionResult> AddClient(NewClient client, [FromQuery(Name = "serviceProviders")]List<int> serviceProviders)
         {
             var newTenant = new IS4Tenant
             {
@@ -40,7 +40,7 @@ namespace IdSrv.Quickstart.Admin
             try
             {
                 await _repo.AddClient(newTenant);
-                UpdateClientConfig(newTenant);
+                UpdateClientConfig(newTenant, serviceProviders);
                 return Ok();
             }catch
             {
@@ -151,13 +151,13 @@ namespace IdSrv.Quickstart.Admin
         }
 
         [ApiExplorerSettings(IgnoreApi = true)]
-        private void UpdateClientConfig(IS4Tenant tenant)
+        private void UpdateClientConfig(IS4Tenant tenant, List<int> serviceProviders)
         {
-            foreach (var client in _configurationDbContext.Clients.AsQueryable().Include("RedirectUris").Include("PostLogoutRedirectUris"))
+            foreach (var client in _configurationDbContext.Clients.AsQueryable().Where(c => serviceProviders.Contains(c.Id)).Include("RedirectUris").Include("PostLogoutRedirectUris"))
             {
-                client.RedirectUris.Add(new ClientRedirectUri() { Client = client, RedirectUri = $"https://{tenant.Name}.localhost:{(client.Id == 1 ? "44372" : "44334")}/signin-oidc-{tenant.TenantId}" });
-                client.PostLogoutRedirectUris.Add(new ClientPostLogoutRedirectUri() { Client = client, PostLogoutRedirectUri = $"https://{tenant.Name}.localhost:{(client.Id == 1 ? "44372" : "44334")}/signout-callback-oidc-{tenant.TenantId}" });
-                _configurationDbContext.Clients.Attach(client).State = EntityState.Modified;
+                    client.RedirectUris.Add(new ClientRedirectUri() { Client = client, RedirectUri = $"https://{tenant.Name}.localhost:{(client.Id == 1 ? "44372" : "44334")}/signin-oidc-{tenant.TenantId}" });
+                    client.PostLogoutRedirectUris.Add(new ClientPostLogoutRedirectUri() { Client = client, PostLogoutRedirectUri = $"https://{tenant.Name}.localhost:{(client.Id == 1 ? "44372" : "44334")}/signout-callback-oidc-{tenant.TenantId}" });
+                    _configurationDbContext.Clients.Attach(client).State = EntityState.Modified;
             }
             _configurationDbContext.SaveChanges();
         }
