7981cd94dc4874c226dcd2ee33ace934f2c45c4f
Elena Ristovska <elena.ristovska@it-labs.com>
2019-04-22 16:05:42 +0200

    Inital verson of Identity Server 4 with asp.net core identity and EF

---
+++ Sensor/IdentityServerAspNetIdentity/Program.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
+using System.Linq;
+
+namespace IdentityServerAspNetIdentity
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
