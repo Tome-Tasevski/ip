43d051111c5729bf044510f9064b42d855e2629d
Elena Ristovska <elena.ristovska@it-labs.com>
2019-05-03 16:56:17 +0200

    fix for logout from ADFS

--- Sensor/IdentityServerAspNetIdentity/Quickstart/AuthSchemeProvider.cs
+++ Sensor/IdentityServerAspNetIdentity/Quickstart/AuthSchemeProvider.cs
@@ -82,7 +82,7 @@ namespace IdentityServerAspNetIdentity.Quickstart
                 {
                     EntityId = samlConfig.IdpEntityId,
                     SingleSignOnEndpoint = new SamlEndpoint(samlConfig.SingleSignOnEndpoint, SamlBindingTypes.HttpRedirect),
-                    SingleLogoutEndpoint = new SamlEndpoint(samlConfig.SingleLogoutEndpoint, SamlBindingTypes.HttpRedirect),
+                    SingleLogoutEndpoint = new SamlEndpoint(samlConfig.SingleLogoutEndpoint, SamlBindingTypes.HttpPost),
                     SigningCertificate = new X509Certificate2(samlConfig.IdpSigningCertificate), //file name
                 };
 
